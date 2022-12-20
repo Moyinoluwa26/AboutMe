cd69e2e4abc92e81b3193041f7a6a589ea296c42
oyejide-board <oyemoyin26@gmail.com>
2022-12-20 22:42:45 +0000

    let's go again

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {faBars, faTimes } from 'react-icons/fa';
+
+export default function  Nav () {
+    return (
+    <div className="flex justify-between items-center w-full h-20 text-white fixed bg-black px-4">
+        <div>
+            <h1 className="text-3xl font-coli"> Moyin </h1>
+        </div>
+        
+    </div>)
+}
