2df405b39d1cc04241a5a0dd75b007c58d309e64
Oyejide <victormoyin435@gmail.com>
2023-01-29 22:32:37 +0100

    a

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Contact = () => {
-    return ( <div name="Contact" className='w-screen h-full bg-black p-4 text-white'>
-        <div className='fle flex-col p-4 justify-center max-w-screen-lg lg mx-auto h-full'>
+    return ( <div name="Contact" className='max-w-screen h-full bg-black p-4 text-white'>
+        <div className='flex flex-col p-4 justify-center max-w-screen-lg lg mx-auto h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500 '>Contact</p>
                 <p className='py-8'>Submit the form below to get in touch with me : </p>
