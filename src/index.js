cd4c38d11b80cc10f369576ba57c879ce870df9d
Oyejide <victormoyin435@gmail.com>
2023-01-29 22:36:11 +0100

    b

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import Contact from './Contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <div className='w-full'> 
+   
     <App />
     <Nav/>
     <Home/>
@@ -21,7 +21,7 @@ root.render(
     <Experience/>
     <Contact/>
     <Links/>
-    </div>
+  
   </React.StrictMode>
 );
 
