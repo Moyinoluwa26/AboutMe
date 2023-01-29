ce3b8c8a3eed596f1f95f868acf740ab15401757
Oyejide <victormoyin435@gmail.com>
2023-01-29 23:03:32 +0100

    d

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const Contact = () => {
-    return ( <div name="Contact" className='w-screen h-full bg-black p-10 text-white'>
+    return ( <div name="Contact" className='overscroll-none w-screen h-full bg-black p-10 text-white'>
         <div className='flex flex-col p-4 justify-center max-w-screen-lg lg mx-auto h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500 '>Contact</p>
