ac4dc1a82d2fa4dea854f10ac4bd63876ecb5cc1
Oyejide <victormoyin435@gmail.com>
2023-02-04 00:25:25 +0100

    got it

--- src/myApp.js
+++ src/myApp.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import Nav from './Nav';
 
-/*import Links from './Links';
+import Links from './Links';
 
 import Portfolio from './Portfolio';
 import Experience from './Experience';
-import Contact from './Contact';*/
+import Contact from './Contact';
 import Home from './head';
-//import About from './About';
+import About from './About';
 
 
 function MyApp() {
-    return ( <div className='w-full'>
+    return ( <div className='overflow-hidden'>
 <Nav/>
 <Home/>
+<About/>
+<Portfolio/>
+<Experience/>
+<Contact/>
+<Links/>
+
 
 
     
